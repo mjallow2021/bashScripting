418fad64f1e567b8f64500c39e9a7f14a2af8bb2
Mohamad <96891774+mjallow2021@users.noreply.github.com>
2023-05-15 17:48:02 +0100

    Update 02-Comments.sh

--- 02-Comments.sh
+++ 02-Comments.sh
@@ -2,7 +2,7 @@
 # This is a comment
 # comments help you make notes/explain in your script
 # like the line below prints Hello World, to print the one below it take of the hashtag
-echo  "Hello World!"
+echo  "Hello World! \n"
 
 
 # echo "You can only seem me if you read the comments"
